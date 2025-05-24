3a2d1cdf5536fafd2144419d21445b54a0fb74f9
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2025-05-24 10:13:52 -0400

    Adding forms to send the messagege

--- website.py
+++ website.py
@@ -2,26 +2,11 @@ from flask import Flask, render_template
 import socket
 
 app = Flask(__name__)
+ip_address = "10.14.3.8"
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/server")
-def server():
-    ip_address = '10.14.3.6'
-    port = 5555
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
-
-    server.bind((ip_address, port))
-    server.listen(1)
-
-    return render_template("server.html", ip_address=ip_address, port=port)
-
-@app.route("/client")
-def client():
-    return render_template("client.html")
+    return render_template("index.html",ip_address=ip_address)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=ip_address, port=5000)
