e1b24b3287b6a66f358e76db43eddf8e75cc35be
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2025-05-31 09:34:54 -0400

    More modifications.

--- website.py
+++ website.py
@@ -3,23 +3,25 @@ import socket
 
 app = Flask(__name__)
 ip_address = "10.14.3.8"
+response = None
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    global response
     if request.method == "POST":
-        ip_address = request.form.get("ip_address")
+        #target_ip = request.form.get("target_ip")
         message = request.form.get("message")
-        response = {"target_ip": ip_address, "message": message}
+        response = response + "\n" + message
    
     return render_template("index.html",ip_address=ip_address, response=response)
 
-@app.route("/send_message", methods=["POST"])
-def send_message():
-    target_ip = request.form.get("target_ip")
-    message = request.form.get("message")
+#@app.route("/send_message", methods=["POST"])
+#def send_message():
+#    target_ip = request.form.get("target_ip")
+#    message = request.form.get("message")
     
-    # For now, just return the data as a response
-    return jsonify({"target_ip": target_ip, "message": message})
+#    # For now, just return the data as a response
+#    return jsonify({"target_ip": target_ip, "message": message})
 
 
 if __name__ == "__main__":
